0511db29152a5ec6dbe13dd09afe1435dba03606
GWFrank <frank910311@gmail.com>
2020-12-22 18:11:03 +0800

    some more problems done

---
+++ p232.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+int is_leap(int year){
+    if (year%400 == 0) return 1;
+    if (year%100 == 0) return 0;
+    if (year%4 == 0) return 1;
+    return 0;
+}
+
+int calc_day(int month, int day, int year, int first_d){
+    
+    int day_diff = 0;
+    int is_leap_year = is_leap(year);
+    if (month > 2 && is_leap_year) {
+        day_diff++;
+    }
+    
+    int m_days[13] = {-1
+        , 31, 28, 31, 30, 31, 30
+        , 31, 31, 30, 31, 30, 31
+    };
+
+    for (int i=1; i<month; i++){
+        day_diff += m_days[i];
+    }
+    day_diff += day-1;
+
+    int ans_day = (first_d + day_diff)%7;
+
+    return ans_day;
+}
+
+int main(){
+    int init_year, init_day = -1;
+    scanf("%d%d", &init_year, &init_day);
+
+    int cases = -1;
+    scanf("%d", &cases);
+
+    int m_days[13] = {-1
+        , 31, 28, 31, 30, 31, 30
+        , 31, 31, 30, 31, 30, 31
+    };
+
+    int leap_m_days[13] = {-1
+        , 31, 29, 31, 30, 31, 30
+        , 31, 31, 30, 31, 30, 31
+    };
+
+    int is_leap_year = is_leap(init_year);
+
+    for (int i=0; i<cases; i++) {
+        
+        int case_m, case_d = -1;
+        scanf("%d%d", &case_m, &case_d);
+        
+        if (case_m > 12 || case_m < 1) {
+           printf("-1\n");
+           continue; 
+        }
+        
+        if (is_leap_year) {
+            if (case_d > leap_m_days[case_m] || case_d < 0) {
+                printf("-2\n");
+                continue;
+            }
+        } else if (!is_leap_year) {
+            if (case_d > m_days[case_m] || case_d < 0) {
+                printf("-2\n");
+                continue;
+            }
+        }
+
+        printf("%d\n", calc_day(case_m, case_d, init_year, init_day));
+    }
+
+
+    return 0;
+}
--- p97.c
+++ p97.c
@@ -8,25 +8,24 @@ int abs(int num){
     }
 }
 
-int calc_dist(int bike[2], int lot[2]){
+int calc_dist(int bike[2], int *lot){
     return abs(bike[0]-lot[0]) + abs(bike[1]-lot[1]);
 }
 
-int rank(int bike[2], int lots[11][2], int lot_num){
-    int scores[11] = {0};
+void rank_lots(int bike[2], int *lots[11], int lot_num){
+    int dists[11] = {0};
 
     for(int i=0; i<lot_num; i++){
-        scores[i] = calc_dist(bike, lots[i]);
+        dists[i] = calc_dist(bike, lots[i]);
     }
 
     for(int i=0; i<lot_num; i++){
-        for(int j=i; j<lot_num; j++){
+        for(int j=i+1; j<lot_num; j++){
             
             int need_swap = 0;
-            if(scores[i] > scores[j]){
+            if(dists[i] > dists[j]){
                 need_swap = 1;
-            }else if(scores[i] == scores[j]){
-
+            }else if(dists[i] == dists[j]){
                 if(lots[i][0] > lots[j][0]){
                     need_swap = 1;
                 }else if(lots[i][0] == lots[j][0]){
@@ -38,43 +37,55 @@ int rank(int bike[2], int lots[11][2], int lot_num){
             }
 
             if(need_swap){
-                int tmp_sc = scores[i];
-                scores[i] = scores[j];
-                scores[j] = tmp_sc;
+                int tmp_d = dists[i];
+                dists[i] = dists[j];
+                dists[j] = tmp_d;
                 
-                int tmp_lot[2] = lots[i][2];
-                lots[i][2] = lots[j][2];
-                lots[j][2] = tmp_lot;
+                int *tmp_lot = lots[i];
+                lots[i] = lots[j];
+                lots[j] = tmp_lot;
             }
-
-
         }
     }
-
-    return 0;
 }
 
 int main(){
     int lot_num = 0;
     scanf("%d", &lot_num);
     
-    int lots[11][2] = {0};
-    int lot_caps[11] = {0};
+    int lots[11][4] = {0};
+    int *lots_ptrs[11] = {NULL};
+    int occupied[11] = {-1};
+
     for(int i=0; i<lot_num; i++){
-        scanf("%d%d%d", lots[i][0], lots[i][1], lot_caps[i]);
+        scanf("%d%d%d", &lots[i][0], &lots[i][1], &lots[i][2]);
+        lots[i][3] = i;
+        lots_ptrs[i] = &lots[i][0];
+        occupied[i] = 0;
     }
 
     int bike_num = 0;
     scanf("%d", &bike_num);
 
-    int bikes[10005][2] = {0};
+    int bikes[100005][2] = {0};
     for(int i=0; i<bike_num; i++){
-        scanf("%d%d", bikes[i][0], bikes[i][1]);
+        scanf("%d%d", &bikes[i][0], &bikes[i][1]);
     }
 
     for(int i=0; i<bike_num; i++){
-
+        rank_lots(bikes[i], lots_ptrs, lot_num);
+        for(int j=0; j<lot_num; j++){
+            if(lots_ptrs[j][2] > 0){
+                lots_ptrs[j][2] -= 1;
+                occupied[lots_ptrs[j][3]] += 1;
+                // occupied[j] += 1;
+                break;
+            }
+        }
     }
 
+    for(int i =0; i<lot_num; i++){
+        printf("%d\n", occupied[i]);
+    }
     return 0;
 }
